82f51352e809183f556a843cf7529c12db74f04e
Megan Moore <meganmoore.dev@gmail.com>
2023-09-13 09:59:31 -0400

    Add project files.

---
+++ PracticeATMProject/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeATMProject
+{
+    internal class Customer
+    {
+        public int ID { get; set; }
+        [StringLength(30)] public string Name { get; set; } = String.Empty;
+        public int CardCode { get; set; }
+        public int PinCode { get; set; }
+        [Column(TypeName = "datetime")] public DateTime LastTransactionDate { get; set; }
+        [Column(TypeName = "datetime")] public DateTime CreationDate { get; set; } = DateTime.Now;
+        [Column(TypeName = "datetime")] public DateTime ModifiedDate { get; set; }
+
+    }
+}
---
+++ PracticeATMProject/Program.cs
@@ -0,0 +1,50 @@
+﻿using PracticeATMProject;
+using System.Text.Json;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        const string baseurl = "http://localhost:5555";
+
+        JsonSerializerOptions joptions = new JsonSerializerOptions()
+        {
+            PropertyNameCaseInsensitive = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = true
+        };
+        HttpClient http = new HttpClient();
+
+        var jsonresponse = await GetUsersAsync(http, joptions); //Make a call
+        var customers = jsonresponse.DataReturned as IEnumerable<Customer>;
+
+        foreach (var c in customers)
+        {
+            Console.WriteLine($"{c.Name}");
+        }
+
+        async Task<JsonResponse> GetUsersAsync(HttpClient http, JsonSerializerOptions jsonOptions)
+        {
+            HttpRequestMessage req = new HttpRequestMessage(HttpMethod.Get, $"{baseurl}/api/customers");//calling what you want
+            HttpResponseMessage resp = await http.SendAsync(req);
+            Console.WriteLine($"Http ErrorCode is {resp.StatusCode}");
+            if (resp.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+
+            }
+            var json = await resp.Content.ReadAsStringAsync();
+            var customers = (IEnumerable<Customer>?)JsonSerializer.Deserialize(json, typeof(IEnumerable<Customer>), jsonOptions);
+            if (customers is null)
+            {
+                throw new Exception();
+            }
+
+            return new JsonResponse()
+            {
+                HttpStatusCode = (int)resp.StatusCode,
+                DataReturned = customers
+            };
+
+        }
+    }
+}
